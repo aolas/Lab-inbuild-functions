f4b20fa17c182f824f68742af2372dcbb02c47ec
Ramunas Daukas <ramunas.daukas@gmail.com>
2021-02-23 09:58:36 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def inputData():
+    # Use a breakpoint in the code line below to debug your script.
+    input_string = input("Enter string of numbers seperated by , : ")
+    test = input_string.split(",");
+    print(test)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    inputData()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
