bf56fc3e390fd2a8f1645b5306c91967ae40c6c1
Ramunas Daukas <ramunas.daukas@gmail.com>
2021-02-23 14:06:13 +0200

    all done

--- main.py
+++ main.py
@@ -47,8 +47,55 @@ class DataManipulation:
                 greater_than_10.append(input_list[ind])
         print("Items greater than 10", greater_than_10)
 # Press the green button in the gutter to run the script.
+class Data_Manipulation_Part2:
+    """Sort the  list   by    item     length in ascending and descending    order."""
+    def list_sort(self):
+        sort_me = ["Kaunas", "Vilnius", "Alytus", "Klaipeda","as" ,"Varena", "Druskininkai", "Klaipeda"]
+        sorted_list = sorted(sort_me, key=len)
+        #sort_me.sort(key=len)
+        print("before sort:", sort_me)
+        print("after sort", sorted_list)
+
+class Data_Manipulation_Part3:
+    """Perform list element transformation: 1. by using formula: x = x * (x - 10), 2. square each element."""
+    def data_manipulation(self):
+        my_list = [12, 45, 23, 56, -546, 34]
+        print(my_list)
+        for ind in range(0, len(my_list)):
+            my_list[ind] = my_list[ind]* (my_list[ind] -10)
+        print("After modification by formula x=x*(x-10):", my_list)
+        for ind in range(0, len(my_list)):
+            my_list[ind] = my_list[ind]**2
+        print("After square:", my_list)
+class Data_Manipulation_Part4:
+    """4. Sort the dictionary according to the second item in the value list in ascending and descending order. """
+    def data_manipulation(self):
+        sort_me = {
+            "a": [7, 1, 9],
+            "b": [8, -4, 3],
+            "c": [9, 10, 151],
+            "d": [-3, 9, 11]
+        }
+        sort_2 = {
+            "a": 1,
+            "b": -5,
+            "c": 55,
+            "d": 11
+        }
+        print("Before ", sort_me)
+        #dict(sorted(sort_me.items(), key=lambda item: item[1]))
+        aa = sorted(sort_me.items(), key=lambda x: x[1][1])    # soting by item second value
+        print("Sort me dictionary", aa)
+
+
 if __name__ == '__main__':
-    data = DataManipulation()
-    data.inputData()
-    #data.data_analyzis([1,2,3,5,9,2,4,6])
+    #part1 = DataManipulation()
+    #part1.inputData()
+    #part2 = Data_Manipulation_Part2()
+    #part2.list_sort()
+    #part3 = Data_Manipulation_Part3()
+    #part3.data_manipulation()
+    part4 = Data_Manipulation_Part4()
+    part4.data_manipulation()
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
